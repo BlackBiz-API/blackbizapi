4950f9d1437a0c4d23062aea534d6237d3b92c1d
Uthman Olomide <generaluthy@gmail.com>
2023-04-08 11:15:10 -0400

    main

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,10 +17,6 @@ function about() {
                         </div>
                             <p>The API provides a RESTful interface that allows developers to access a wide range of business information, including names, categories, addresses, and more. This makes it a powerful tool for businesses to reach a wider audience, increase visibility, and build stronger relationships with customers.</p>
                             <p>In addition, the API provides accurate, up-to-date information, helping businesses connect with potential customers and grow their customer base. By supporting black-owned businesses, we aim to create a more equitable and inclusive economy.</p>
-                            <p>With Black Biz API, you have the opportunity to play a role in this mission. On this page, you'll find a button to make a donation, which will help us continue to provide this valuable resource to the community.</p>
-                            <div className="cta-holder d-flex w-100 justify-content-center align-items-center">
-                            <Link href="/" className="mx-5 bg_btn_color">Donate</Link>
-                        </div>
                     </div>
                 </div>
             </div>
